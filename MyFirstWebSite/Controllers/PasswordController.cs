3397d4d7a14c830b7038c0cf8c4f258c9e3d9d14
tmWebApi <tami.malky.webapi@gmail.com>
2022-12-25 23:08:21 +0200

    change name of startProject from MyFirstWebSite to MyWebsite

--- MyFirstWebSite/Controllers/PasswordController.cs
+++ MyFirstWebSite/Controllers/PasswordController.cs
@@ -4,7 +4,7 @@ using Zxcvbn;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MyFirstWebSite.Controllers
+namespace MyWebsite.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- MyFirstWebSite/Controllers/ProductController.cs
+++ MyFirstWebSite/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MyFirstWebSite.Controllers
+namespace MyWebsite.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
